4e0853f3f947affc924b4cfb71c469f9aff3028b
Abbey <savarb@gmail.com>
2020-04-22 12:41:18 +0000

    Revert "Revert "Updated code""
    This reverts commit a8877bd6ff36c2b856cfcad85ff6fe3dd1ce50d6.

--- contract.go
+++ contract.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"time"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
@@ -32,15 +30,15 @@ type Contract struct {
 }
 
 //Put adds a new key with value to the world state
-func (contract *Contract) Put(ctx contractapi.TransactionContextInterface, mobile string, name string, email string, network string) (err error) {
+func (contract *Contract) Put(ctx contractapi.TransactionContextInterface, mobile string, name string, email string, network string) {
 
 	if mobile == "" {
-		err = errors.New("Mobile can not be empty")
+		// err = errors.New("Mobile can not be empty")
 		return
 	}
 
 	if name == "" {
-		err = errors.New("Name can not be empty")
+		// err = errors.New("Name can not be empty")
 		return
 	}
 
@@ -50,45 +48,49 @@ func (contract *Contract) Put(ctx contractapi.TransactionContextInterface, mobil
 	obj.Email = email
 	obj.Network = network
 
-	if obj.Created, err = GetTimestamp(ctx); err != nil {
-		return
-	}
+	// if obj.Created, err = GetTimestamp(ctx); err != nil {
+	// 	return
+	// }
 
-	if obj.Createdby, err = GetCallerID(ctx); err != nil {
-		return
-	}
+	// if obj.Createdby, err = GetCallerID(ctx); err != nil {
+	// 	return
+	// }
 
 	key := mobile
 	objBytes, _ := json.Marshal(obj)
-	err = ctx.GetStub().PutState(key, []byte(objBytes))
+	ctx.GetStub().PutState(key, []byte(objBytes))
 	return
 }
 
 //Get retrieves the value linked to a key from the world state
-func (contract *Contract) Get(ctx contractapi.TransactionContextInterface, key string) (*ContractObj, error) {
+func (contract *Contract) Get(ctx contractapi.TransactionContextInterface, key string) *ContractObj {
 
 	existingObj, err := ctx.GetStub().GetState(key)
 	if err != nil {
-		return nil, err
+		return nil
+		// return nil, err
 	}
 
 	if existingObj == nil {
-		return nil, fmt.Errorf("Cannot read world state pair with key %s. Does not exist", key)
+		return nil
+		// return nil, fmt.Errorf("Cannot read world state pair with key %s. Does not exist", key)
 	}
 
 	ContractObj := new(ContractObj)
 	if err := json.Unmarshal(existingObj, ContractObj); err != nil {
-		return nil, fmt.Errorf("Data retrieved from world state for key %s was not of type ContractObj", key)
+		return nil
+		// return nil, fmt.Errorf("Data retrieved from world state for key %s was not of type ContractObj", key)
 	}
-	return ContractObj, nil
+	return ContractObj
 }
 
 //History retrieves the history linked to a key from the world state
-func (contract *Contract) History(ctx contractapi.TransactionContextInterface, key string) ([]ContractHistory, error) {
+func (contract *Contract) History(ctx contractapi.TransactionContextInterface, key string) []ContractHistory {
 
 	iter, err := ctx.GetStub().GetHistoryForKey(key)
 	if err != nil {
-		return nil, err
+		return nil
+		// return nil, err
 	}
 	defer func() { _ = iter.Close() }()
 
@@ -96,12 +98,14 @@ func (contract *Contract) History(ctx contractapi.TransactionContextInterface, k
 	for iter.HasNext() {
 		state, err := iter.Next()
 		if err != nil {
-			return nil, err
+			return nil
+			// return nil, err
 		}
 
 		entryObj := new(ContractObj)
 		if errNew := json.Unmarshal(state.Value, entryObj); errNew != nil {
-			return nil, errNew
+			return nil
+			// return nil, errNew
 		}
 
 		entry := ContractHistory{
@@ -112,5 +116,5 @@ func (contract *Contract) History(ctx contractapi.TransactionContextInterface, k
 
 		results = append(results, entry)
 	}
-	return results, nil
+	return results
 }
