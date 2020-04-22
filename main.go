3821d12b9f7b812da57dfe931582514089f74bf7
Abbey <savarb@gmail.com>
2020-04-22 12:27:26 +0000

    Updated test

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+func main() {
+
+	contract := new(Contract)
+
+	cc, err := contractapi.NewChaincode(contract)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if err := cc.Start(); err != nil {
+		panic(err.Error())
+	}
+}
